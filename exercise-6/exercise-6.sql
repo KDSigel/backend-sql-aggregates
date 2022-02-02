e634fbb3fe8136f754b6cfcf49341325b55447b4
KDSigel <heykarl@gmail.com>
2022-02-02 14:31:30 -0800

    added the code for first 6 exercises

--- exercise-6/exercise-6.sql
+++ exercise-6/exercise-6.sql
@@ -1,3 +1,15 @@
 -- customer ranking by total amount spent
 -- include email and rank
 -- use RANK ORDER BY the SUM of the amount in descending order
+
+SELECT
+customer.email,
+RANK() OVER (ORDER BY SUM(payment.amount)DESC)
+FROM
+payment
+JOIN rental
+ON rental.rental_id = payment.rental_id
+JOIN customer
+ON customer.customer_id = rental.customer_id
+GROUP BY customer.email
+ORDER BY RANK ASC
