531946790b17ce6a314d21451761ff10c2ee7ab8
KDSigel <heykarl@gmail.com>
2022-02-02 15:10:27 -0800

    wrote exercises up to 8

--- exercise-7/exercise-7.sql
+++ exercise-7/exercise-7.sql
@@ -1,2 +1,16 @@
 -- customer ranking by total film time watched
 -- similar to exercise 6
+
+SELECT
+customer.email,
+RANK() OVER (ORDER BY SUM(film.length)DESC)
+FROM
+film
+JOIN inventory
+ON inventory.film_id = film.film_id
+JOIN rental
+ON rental.inventory_id = inventory.inventory_id
+JOIN customer
+ON customer.customer_id = rental.customer_id
+GROUP BY customer.email
+ORDER BY RANK ASC
